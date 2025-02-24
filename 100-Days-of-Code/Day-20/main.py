9c41a0aef8241efb6bc45c0cce08f8e1fd91fb12
Jaya-sai-kiran <jaysaikiran18@gmail.com>
2025-02-24 16:07:54 +0530

    Day-20 added

---
+++ 100-Days-of-Code/Day-20/main.py
@@ -0,0 +1,21 @@
+#Snake game-->Initial Part
+from snake import Snake,My_screen
+
+My_screen.setup(width=600,height=600)
+My_screen.bgcolor("black")
+My_screen.title("My Snake Game")
+My_screen.tracer(0)
+
+sn=Snake()
+My_screen.listen()
+My_screen.onkey(sn.up,"Up")
+My_screen.onkey(sn.down,"Down")
+My_screen.onkey(sn.left,"Left")
+My_screen.onkey(sn.right,"Right")
+
+
+game_is_on=True
+while game_is_on:
+    sn.move_forward()
+
+My_screen.exitonclick()
---
+++ 100-Days-of-Code/Day-20/snake.py
@@ -0,0 +1,56 @@
+from turtle import Turtle,Screen
+import time
+
+My_screen=Screen()
+DISTANCE=20
+SLEEP_TIME=0.1
+UP=90
+DOWN=270
+LEFT=180
+RIGHT=0
+
+class Snake:
+    def __init__(self):
+        self.snakes = []
+        x = 0
+        for i in range(3):
+            self.create_snake()
+            self.snakes[i].goto(x=x, y=0)
+            x+=-20
+            My_screen.update()
+        self.head = self.snakes[0]
+
+    def create_snake(self):
+        tim = Turtle("square")
+        tim.penup()
+        tim.color("white")
+        self.snakes.append(tim)
+
+    def body_move(self):
+        for i in range(len(self.snakes) - 1, 0, -1):
+            x = self.snakes[i - 1].xcor()
+            y = self.snakes[i - 1].ycor()
+            self.snakes[i].goto(x=x, y=y)
+
+    def move_forward(self):
+        self.body_move()
+        self.head.forward(DISTANCE)
+        My_screen.update()
+        time.sleep(SLEEP_TIME)
+
+    def up(self):
+        if self.head.heading() != DOWN:
+            self.head.setheading(UP)
+
+    def down(self):
+        if self.head.heading() != UP:
+            self.head.setheading(DOWN)
+
+    def left(self):
+        if self.head.heading() != RIGHT:
+            self.head.setheading(LEFT)
+
+    def right(self):
+        if self.head.heading() != LEFT:
+            self.head.setheading(RIGHT)
+
